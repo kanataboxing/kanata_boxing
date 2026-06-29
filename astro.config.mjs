9d55ed017a46f7050e3a5c103c99ed0911cea18d
kanataboxing <kanataboxing@gmail.com>
2026-06-29 19:20:01 -0400

    fixing site config

--- astro.config.mjs
+++ astro.config.mjs
@@ -9,6 +9,6 @@ export default defineConfig({
   vite: {
     plugins: [tailwindcss()]
   },
-  site: 'https://kanataboxing.github.io',
+  site: 'https://kanataboxing.ca',
   base: '/kanata_boxing',
 });
